a88709c5f52620f4a3f405ec28a0b3554c37e05d
MiniGT <bendanillo539@gmail.com>
2025-01-18 22:33:53 +0800

    getDieData logic

---
+++ src/utils/getDieData.ts
@@ -0,0 +1,19 @@
+import { DieData } from "../types/dieData.types";
+
+// get die data
+export const getDieData = (): DieData[] => {
+  const DieData = []
+  for (let i = 0; i < 10; i++) {
+    DieData.push(
+      {
+        id: i,
+        value: Math.floor(Math.random() * 10) + 1, 
+        selected: false
+      }
+    )
+  }
+  console.log(DieData)
+  return DieData
+};
+
+
